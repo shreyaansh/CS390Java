02b9e464e06ce8ab536a45f90e7f8d21dfa325f6
shreyaansh <sbassi@purdue.edu>
2016-11-06 20:04:40 -0500

    Description Taing updated

--- src/Crawler.java
+++ src/Crawler.java
@@ -5,6 +5,7 @@ import java.sql.*;
 import java.util.*;
 
 import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -21,7 +22,7 @@ public class Crawler
 
 	public void readProperties() throws IOException {
       		props = new Properties();
-      		FileInputStream in = new FileInputStream("database.properties");
+      		FileInputStream in = new FileInputStream("src/database.properties");
       		props.load(in);
       		in.close();
 	}
@@ -66,27 +67,90 @@ public class Crawler
 		return false;
 	}
 
-	public void insertURLInDB( String url) throws SQLException, IOException {
+	public void insertURLInDB(String url) throws SQLException, IOException {
+		// Check if url returns an HTML file or not
+		try {
+			Document doc = Jsoup.connect(url).get();
+		} catch (UnsupportedMimeTypeException m) {
+			return;
+		}
+
 		Statement stat = connection.createStatement();
 		String query = "INSERT INTO urls VALUES ('"+urlID+"','"+url+"','')";
 		System.out.println("Executing " + query);
 		stat.executeUpdate( query );
+		fetchDescription(url, urlID);
 		urlID++;
+	}
+
+	public void insertDescInDB(String desc, int urlID) throws SQLException, IOException {
+		Statement stat = connection.createStatement();
+		String query = "UPDATE urls SET description=\"" + desc + "\" WHERE urlid="+ urlID;
+		System.out.println("Executing " + query);
+		stat.executeUpdate(query);
+	}
+
+	public String getMetaContent(Document doc, String attribute) {
+		Elements elements = doc.select("meta[name="+attribute+"]");
+		for (Element element: elements) {
+			String s = element.attr("content");
+			if (s != null) return s;
+		}
+		elements = doc.select("meta[property=" + attribute + "]");
+		for (Element element : elements) {
+			final String s = element.attr("content");
+			if (s != null) return s;
+		}
+		return null;
+	}
+
+	public void fetchDescription (String url, int urlID) throws SQLException, IOException {
+		Document doc = Jsoup.connect(url).get();
+
+		// Get the description and store it
+		String title = doc.title();
+
+		String description = getMetaContent(doc, "description");
+		if (description == null) description = getMetaContent(doc, "og:description");
+
+		if (description == null) description = "";
+
+		String text = description + " " + title;
+
+		text.toLowerCase();
+		String words[] = text.split("\\P{Alpha}+");
+
+		String desc = "";
+		String tempDesc = "";
+		for (String word: words) {
+			tempDesc = tempDesc + word + " ";
+			if (tempDesc.length() < 200) desc = tempDesc;
+			else break;
+		}
+		System.out.println("Words: " + desc);
+
+		insertDescInDB(desc, urlID);
 	}
 
    	public void fetchURL(String url) {
 		try {
+			System.out.println("Before opening");
 			Document doc = Jsoup.connect(url).get();
 
+			// Selecting the links on the page
 			Elements links = doc.select("a[href]");
 
+			// Checking and storing the links in the database
 			for (Element link: links) {
 				String absUrl = link.attr("abs:href").toString();
 
+				// How to check if URL is not http or https
+				if (!absUrl.substring(0, 4).equals("http") || !absUrl.substring(0, 5).equals("https")) continue;
+
+				// If URL doesn't exist in DB then add it
 				if (!urlInDB(absUrl)) {
 					insertURLInDB(absUrl);
 				}
-
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
